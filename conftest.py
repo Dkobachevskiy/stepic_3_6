ea8a2068171810086b45012e9e7db1e4d78c7073
Кобачевский Денис <kobacevskiydenis@MacBook-Air-Kobacevskij.local>
2022-01-14 18:40:04 +0300

    finaly task ready and have working assert

---
+++ conftest.py
@@ -0,0 +1,31 @@
+import pytest, time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--language',
+        action='store',
+        default=None,
+        help="Choose language"
+    )
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option(
+        'prefs',
+        {'intl.accept_languages': user_language}
+    )
+    if user_language:
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(5)
+        time.sleep(3)
+    else:
+        raise pytest.UsageError("please choose --language from list: ar, ca, cs, da, de, en-gb, el, es, fi, fr, it, ko, nl, pl, pt, pt-br, ro, ru, sk, uk, zh-hans")
+    print("\nstart chrome browser for test..")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,6 @@
+link = f" http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
+
+
+def test_basket_button(browser):
+    browser.get(link)
+    assert browser.find_elements_by_css_selector(".btn-add-to-basket"), 'basket button not found'
